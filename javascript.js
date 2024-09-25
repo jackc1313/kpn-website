b069d6883edfdbe7788974668673540cb1d15cbc
Jacopo Cariato <jacopo.cariato98gmail.com>
2024-09-25 19:54:23 +0200

    created a js event for the button

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('register-button');
+
+button.addEventListener('click', function() {
+    window.open('https://form.jotform.com/231194277337359?fbclid=PAAabJxPsUw9kc5Msc-h0RMv523Wqu-RhXhEhfRIDNv3m4tp2ZFnBhwiiXcM8', '_blank')
+});
