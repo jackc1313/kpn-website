f057eb43999e5bb33722fb951613a23570bc5b95
Jacopo Cariato <jacopo.cariato98gmail.com>
2024-09-27 18:27:34 +0200

    added a hamburger menu for smaller screens

--- javascript.js
+++ javascript.js
@@ -3,3 +3,8 @@ const button = document.getElementById('register-button');
 button.addEventListener('click', function() {
     window.open('https://form.jotform.com/231194277337359?fbclid=PAAabJxPsUw9kc5Msc-h0RMv523Wqu-RhXhEhfRIDNv3m4tp2ZFnBhwiiXcM8', '_blank')
 });
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+  }  
